dcbccd17a958b30b60d1bf143a28f7c411f6f0e2
garethS-debug <garethjswarte@gmail.com>
2021-11-16 13:32:20 +0000

    Continued work on bridge puzzle

--- Assets/Scripts/Puzzles/Bridge/BridgePuzzle_CheckForPiece.cs
+++ Assets/Scripts/Puzzles/Bridge/BridgePuzzle_CheckForPiece.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
 
 public class BridgePuzzle_CheckForPiece : MonoBehaviour
 {
@@ -16,6 +19,10 @@ public class BridgePuzzle_CheckForPiece : MonoBehaviour
     public GameObject brokenBridge;
     //public GameObject brokenBridgeMesh;
 
+
+    [Header("Photon Settings")]
+    PhotonView PV;                                              //Setting up photon view 
+
     public bool DebugOutSideOfNetwork = false;
 
     public void Start()
@@ -23,6 +30,7 @@ public class BridgePuzzle_CheckForPiece : MonoBehaviour
 
         DebugOutSideOfNetwork = false;
         missingPieceBoxCollder.SetActive(false);
+        PV = this.gameObject.GetComponent<PhotonView>();        //Get the photonview on the player
     }
 
     private void Update()
@@ -47,15 +55,14 @@ public class BridgePuzzle_CheckForPiece : MonoBehaviour
                if (inventory.inventory.Container[i].item == missingItem.item)
                 {
 
+                    PV.RPC("RPC_PropChangeModel", RpcTarget.All/* tempHit.GetPhotonView().viewID*/ );
 
                     //Container[i].AddAmount(_amount);
                     //  hasItem = true;
 
                     // if (_item.name == "GoldenKey")
                     // {
-                    brokenBridge.GetComponent<MeshFilter>().mesh = fixedBridgeMesh;
-                        Debug.Log("2 golden keys");
-                    missingPieceBoxCollder.SetActive(true);
+                  
                     // }
                 }
 
@@ -89,4 +96,23 @@ public class BridgePuzzle_CheckForPiece : MonoBehaviour
 
         }
     }
+
+
+    [PunRPC]
+    void RPC_PropChangeModel()
+    {
+        //if (!PV.IsMine)
+        //   return;
+
+        //PhotonView targetPV = PhotonView.Find(targetPropID);
+
+        // if (targetPV.gameObject == null)
+        //     return;
+
+        brokenBridge.GetComponent<MeshFilter>().mesh = fixedBridgeMesh;
+        Debug.Log("2 golden keys");
+        missingPieceBoxCollder.SetActive(true);
+    }
+
+
 }
