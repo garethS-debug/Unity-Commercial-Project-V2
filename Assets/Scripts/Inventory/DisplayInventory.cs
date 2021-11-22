2a42e1f7f1980edcefc54509e2e854d02cc8005f
Claire Peuz <clairepeuz@yahoo.fr>
2021-11-22 21:55:21 +0000

    Grief bar, gate, inventory update

--- Assets/Scripts/Inventory/DisplayInventory.cs
+++ Assets/Scripts/Inventory/DisplayInventory.cs
@@ -5,7 +5,8 @@ using TMPro;
 
 public class DisplayInventory : MonoBehaviour
 {
-    public InventoryObject inventory;
+    public InventoryObject inventoryHuman;
+    public InventoryObject inventoryGhost;
     public int xStart;
     public int xSpaceBetweenItems;
     public int yStart;
@@ -16,45 +17,48 @@ public class DisplayInventory : MonoBehaviour
 
     private void Start()
     {
-        CreateDisplay();
+        CreateDisplay(inventoryGhost);
+        CreateDisplay(inventoryHuman);
     }
 
     public void Update()
     {
-        UpdateDisplay();
+        UpdateDisplay(inventoryGhost);
+        UpdateDisplay(inventoryHuman);
     }
 
-    public void UpdateDisplay()
-    {
-        for (int i = 0; i < inventory.Container.Count; i++)
-        {
-            if (itemsDisplayed.ContainsKey(inventory.Container[i]))
-            {
-                itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
-            }
-            else
-            {
-                var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
-                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
-                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
-                itemsDisplayed.Add(inventory.Container[i], obj);
-            }
-        }
-    }
 
-    public void CreateDisplay()
+    public void CreateDisplay(InventoryObject inv)
     {
-        for (int i = 0; i < inventory.Container.Count; i++)
+        for (int i = 0; i < inv.Container.Count; i++)
         {
-            var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+            var obj = Instantiate(inv.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
-            obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
-            itemsDisplayed.Add(inventory.Container[i], obj);
+            obj.GetComponentInChildren<TextMeshProUGUI>().text = inv.Container[i].amount.ToString("n0");
+            itemsDisplayed.Add(inv.Container[i], obj);
         }
     }
 
     public Vector3 GetPosition(int i)
     {
         return new Vector3(xStart + (xSpaceBetweenItems * (i % numberOfColumns)), yStart + (-ySpaceBetweenItems * (i / numberOfColumns)), 0f);
+    }
+
+    public void UpdateDisplay(InventoryObject inv)
+    {
+        for (int i = 0; i < inv.Container.Count; i++)
+        {
+            if (itemsDisplayed.ContainsKey(inv.Container[i]))
+            {
+                itemsDisplayed[inv.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inv.Container[i].amount.ToString("n0");
+            }
+            else
+            {
+                var obj = Instantiate(inv.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = inv.Container[i].amount.ToString("n0");
+                itemsDisplayed.Add(inv.Container[i], obj);
+            }
+        }
     }
 }
