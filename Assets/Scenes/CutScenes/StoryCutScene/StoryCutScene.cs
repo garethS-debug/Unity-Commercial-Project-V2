816efd0b6df55eb64a4b6c00ee08a11386265233
garethS-debug <garethjswarte@gmail.com>
2021-12-06 21:12:10 +0000

    Cutscenes/UI

---
+++ Assets/Scenes/CutScenes/StoryCutScene/StoryCutScene.cs
@@ -0,0 +1,177 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StoryCutScene : MonoBehaviour
+{
+
+    [Header("Camera")]
+    public Camera cutsceneCamera;
+
+    [Header("Train")]
+    public GameObject train;
+    public float trainspeed = 1;
+    [Header("Delays")]
+
+    public float cutSceneDelayAtStart = 2;
+    public float cutSceneDelay = 5;
+
+    [Header("References")]
+    public Animator trainAnimator;
+    public LobbyManager lobbyManager;
+
+    public GameObject trainFollowCube;
+
+    [Header("Intro")]
+    public GameObject intro;
+    public TMP_Text txt;
+    [TextArea]
+    public string story;
+   
+
+    [Header("Debug")]
+    public bool DebugSkipCutScene;
+
+    [Header("UI")]
+    public GameObject skipButton;
+
+    [Header("Co-Courtines")]
+    Coroutine theCutSceneCoRoutine;
+
+
+
+    [Header("Scene Ref")]
+    public SceneReference levelToLoad;
+
+
+    /*
+     * 
+     *   
+  \n    New line
+  \t    Tab
+  \v    Vertical Tab
+  \b    Backspace
+  \r    Carriage return
+  \f    Formfeed
+  \\    Backslash
+  \'    Single quotation mark
+  \"    Double quotation mark
+  \d    Octal
+  \xd    Hexadecimal
+  \ud    Unicode character
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //   trainAnimator.SetBool("StartEntry", false);
+        //Start the coroutine we define below named ExampleCoroutine.
+        if (DebugSkipCutScene == false)
+        {
+            theCutSceneCoRoutine = StartCoroutine(CutSceneCoRoutine());
+        }
+
+        if (DebugSkipCutScene == true)
+        {
+            lobbyManager.OnTriggerSpawnPlayers();
+        }
+
+        skipButton.gameObject.SetActive(true);
+
+
+    }
+
+    void Awake()
+    {
+        //  story
+      //  txt.text = "";
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // cutsceneCamera.transform.LookAt(trainFollowCube.transform);
+        // Move the object forward along its z axis 1 unit/second.
+       train.transform.Translate(Vector3.forward  * Time.deltaTime * trainspeed);
+    }
+
+    IEnumerator CutSceneCoRoutine()
+    {
+        StartCoroutine("PlayText");
+
+        intro.gameObject.SetActive(true);
+        //Print the time of when the function is first called.
+        // Debug.Log("Started IntroSplash Coroutine at timestamp : ".Bold().Color("yellow") + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(cutSceneDelayAtStart);
+        intro.gameObject.SetActive(false);
+        //Print the time of when the function is first called.
+        //  Debug.Log("Started Cutscene Coroutine at timestamp : ".Bold().Color("yellow") + Time.time);
+
+        trainAnimator.SetBool("StartEntry", true);
+
+
+        //After we have waited 5 seconds print the time again.
+        //    Debug.Log("Finished Cutscene Coroutine at timestamp : ".Bold().Color("yellow") + Time.time);
+
+        yield return new WaitForSeconds(cutSceneDelay);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Spawn Player after Cutscene Coroutine at timestamp : ".Bold().Color("yellow") + Time.time);
+
+        skipButton.gameObject.SetActive(false);
+        lobbyManager.OnTriggerSpawnPlayers();
+
+
+    }
+
+
+
+
+
+    IEnumerator PlayText()
+    {
+        foreach (char c in story)
+        {
+            txt.text += c;
+            yield return new WaitForSeconds(0.075f);
+
+            if (c >= story.Length)
+            {
+                print("End Of Story");
+
+            }
+        }
+    }
+
+
+
+    public void SkipTheCutScene()
+    {
+        intro.gameObject.SetActive(false);
+        trainAnimator.SetBool("StartEntry", true);
+
+        StopCoroutine(theCutSceneCoRoutine);
+
+        StartCoroutine(SkipCutscene());
+
+
+
+
+        cutSceneDelayAtStart = 0;
+        cutSceneDelay = 0;
+        skipButton.gameObject.SetActive(false);
+    }
+
+    IEnumerator SkipCutscene()
+    {
+        yield return new WaitForSeconds(3);
+
+        lobbyManager.OnTriggerSpawnPlayers();
+    }
+}
--- Assets/Scripts/Player/GameSetup.cs
+++ Assets/Scripts/Player/GameSetup.cs
@@ -38,7 +38,8 @@ public class GameSetup : MonoBehaviour
 	[Header("SO")]
 	public PlayerSO playerSOData;
 
- 
+	[Header("Tutorial")]
+	public MainMenu_Cutscene tutorialCutscene;
 
 	// Start is called before the first frame update
 	void Start()
@@ -141,22 +142,35 @@ public class GameSetup : MonoBehaviour
 	public void m_START_GAME()
     {
 	 bool isSave =	SaveGameManager.CheckforSaveGame();
-
 	if (isSave)
         {
 		//	Debug.Log("Continue to game with your character...");
 			SaveGameManager.Load();
 
-			UpdatePlayerSaveSO(1);						//1 = multiplayer
+			UpdatePlayerSaveSO(1);                      //1 = multiplayer
 
-			SceneManager.LoadScene(levelToLoad);
+			///---- StartTutorialCutScene--->
+			///SELECT_CHILDCHARACTER();
+			tutorialCutscene.gamesetup = this.gameObject.GetComponent<GameSetup>();
+			tutorialCutscene.continue_StartTutorialCutscene();
+		
 		}
+
+
+
+
 	else
         {
-		//	Debug.Log("Choose A Character");
+			//Moving character select to lobby creation -------------> 
+			/*
 			selectCharacterUI.SetActive(true);
 			StartUI.SetActive(false);
+			*/
 
+			///---- StartTutorialCutScene--->
+			///SELECT_CHILDCHARACTER();
+			tutorialCutscene.gamesetup = this.gameObject.GetComponent<GameSetup>();
+			tutorialCutscene.start_StartTutorialCutscene();
 		}
 
 	}
@@ -171,13 +185,24 @@ public class GameSetup : MonoBehaviour
 			//	Debug.Log("Continue to game with your character...");
 			SaveGameManager.Load();
 			UpdatePlayerSaveSO(2);                  //2 = Single Player
-			SceneManager.LoadScene(levelToLoad);
+
+			///---- StartTutorialCutScene--->
+			///SELECT_CHILDCHARACTER();
+			tutorialCutscene.gamesetup = this.gameObject.GetComponent<GameSetup>();
+			tutorialCutscene.continue_StartTutorialCutscene();
 		}
 		else
 		{
-			//	Debug.Log("Choose A Character");
+			//Moving character select to lobby creation -------------> 
+			/*
 			selectCharacterUI.SetActive(true);
 			StartUI.SetActive(false);
+			*/
+
+			///---- StartTutorialCutScene--->
+			///SELECT_CHILDCHARACTER();
+			tutorialCutscene.gamesetup = this.gameObject.GetComponent<GameSetup>();
+			tutorialCutscene.start_StartTutorialCutscene();
 
 		}
 
@@ -197,5 +222,10 @@ public class GameSetup : MonoBehaviour
 
 		
 
+	}
+
+	public void LoadNextScene()
+    {
+		SceneManager.LoadScene(levelToLoad);
 	}
 }
