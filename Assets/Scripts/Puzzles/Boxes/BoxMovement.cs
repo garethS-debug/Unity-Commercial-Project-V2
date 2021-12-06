6d5c5dcd1aecc654e2f11e8b7f1a6b4a6c734d47
Claire Peuz <clairepeuz@yahoo.fr>
2021-12-06 09:21:56 +0000

    Level 2 progress

--- Assets/Scripts/Puzzles/Boxes/BoxMovement.cs
+++ Assets/Scripts/Puzzles/Boxes/BoxMovement.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class BoxMovement : MonoBehaviour
 {
+
+    public DisplayKey displayKey;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("TargetTrigger"))
         {
-            Debug.Log("one box");
+            displayKey.AddBox(1);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("TargetTrigger"))
+        {
+            displayKey.AddBox(-1);
         }
     }
 }
---
+++ Assets/Scripts/Puzzles/Boxes/DisplayKey.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayKey : MonoBehaviour
+{
+    int countBox = 0;
+
+    void Update()
+    {
+        if (countBox == 2)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void AddBox(int _box)
+    {
+        countBox += _box;
+    }
+}
