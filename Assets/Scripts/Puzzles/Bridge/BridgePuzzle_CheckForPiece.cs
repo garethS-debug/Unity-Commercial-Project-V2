3b6f4ebb7fefac913e4b9e4c8886549912951cfc
garethS-debug <garethjswarte@gmail.com>
2021-12-08 16:34:32 +0000

    Jump fix

--- Assets/Scripts/Puzzles/Bridge/BridgePuzzle_CheckForPiece.cs
+++ Assets/Scripts/Puzzles/Bridge/BridgePuzzle_CheckForPiece.cs
@@ -78,62 +78,48 @@ public class BridgePuzzle_CheckForPiece : MonoBehaviour
 
                 
 
-            if (inventory.inventory.database.Items.Length >= 1)
-            {
-                for (int i = 0; i < inventory.inventory.database.Items.Length ; i++)
-                {
-                    Debug.Log("-------------------");
-                    Debug.Log("Looping through inv");
+                    foreach (InventorySlot item in inventory.inventory.Container.Items)
+                    {
+                        print(item.ID);
+                        Debug.Log("-------------------");
+                        Debug.Log("Looping through inv");
 
-                    //  Debug.Log("inventory.inventory.database.Items[i] = " + inventory.inventory.database.Items[i]);       //Key Object
-                    //   Debug.Log("missingItem.item =  " + missingItem.item);       //Key Object
-                    
-                            if (inventory.inventory.Container.Items.Length >= 1)
+                        if (item.ID == missingItem.item.Id)
+                        {
+                            Debug.Log("-------------------");
+                            Debug.Log("found our boy");
+
+                            if (SceneSettings.Instance.isMultiPlayer == true)
                             {
-                                if (inventory.inventory.Container.Items[i].ID == missingItem.item.Id) //--- Error
-
-                                {
-                                    Debug.Log("-------------------");
-                                    Debug.Log("found our boy");
-
-                                    if (SceneSettings.Instance.isMultiPlayer == true)
-                                    {
-                                        PhotonView photonView = PhotonView.Get(this);
-                                        photonView.RPC("RPC_PropChangeModel", RpcTarget.All/* tempHit.GetPhotonView().viewID*/ );
-                                        photonView.RPC("RPC_ShowKey", RpcTarget.All/* tempHit.GetPhotonView().viewID*/ );
-                                    }
-
-                                    if (SceneSettings.Instance.isSinglePlayer == true)
-                                    {
-                                        FixBridge();
-                                        s_ShowKey();
-                                    }
-
-                                }
-                                else
-                                {
-                                    Debug.Log("Does not match Inventory");
-                                }
-
+                                PhotonView photonView = PhotonView.Get(this);
+                                photonView.RPC("RPC_PropChangeModel", RpcTarget.All/* tempHit.GetPhotonView().viewID*/ );
+                                photonView.RPC("RPC_ShowKey", RpcTarget.All/* tempHit.GetPhotonView().viewID*/ );
                             }
 
-                            else 
+                            if (SceneSettings.Instance.isSinglePlayer == true)
                             {
-                                Debug.LogError("Out of range");
+                                FixBridge();
+                                s_ShowKey();
                             }
-
-
 
                         }
-            }
-
-            else
-                    {
-                        Debug.LogError("Out of range");
+                        else
+                        {
+                            Debug.Log("Does not match Inventory");
+                        }
                     }
+                    
+
+
+
+
+
+
+
+
 
             }
-
+            
 
             //    inventory.inventory
 
