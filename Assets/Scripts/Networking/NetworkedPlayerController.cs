12bdafce0996069faeb187a8082f80ffc3ae5528
garethS-debug <garethjswarte@gmail.com>
2021-12-10 01:06:54 +0000

    Camera Changes

--- Assets/Scripts/Networking/NetworkedPlayerController.cs
+++ Assets/Scripts/Networking/NetworkedPlayerController.cs
@@ -88,6 +88,8 @@ public class NetworkedPlayerController : MonoBehaviour
 	[HideInInspector] public PlayerCameraController _camControll;
 	public bool SpawnTestCam;
 	public CinemachineVirtualCamera vcam;
+	public CinemachineFreeLook fcam;
+
 
 
 	//Vector3 newPOS;
@@ -234,9 +236,21 @@ public class NetworkedPlayerController : MonoBehaviour
 			cam = CameraPrefab.gameObject.transform;
 
 			//V-CAM
-			vcam = cam.GetComponentInChildren<CinemachineVirtualCamera>();
-			vcam.m_Follow = cameraFollowTarget.transform;
-			vcam.m_LookAt = cameraFollowTarget.transform;
+			if (camPrefab.GetComponentInChildren<CinemachineVirtualCamera>())
+			{
+				vcam = cam.GetComponentInChildren<CinemachineVirtualCamera>();
+				vcam.m_Follow = cameraFollowTarget.transform;
+				vcam.m_LookAt = cameraFollowTarget.transform;
+			}
+
+
+			if (camPrefab.GetComponentInChildren<CinemachineFreeLook>())
+            {
+				fcam = cam.GetComponentInChildren<CinemachineFreeLook>();
+				fcam.m_Follow = cameraFollowTarget.transform;
+				fcam.m_LookAt = cameraFollowTarget.transform;
+			}
+
 
 
 			//Display the parent's name in the console.
@@ -413,7 +427,11 @@ public class NetworkedPlayerController : MonoBehaviour
 			 jumpDirForward = moveDir.normalized.z; //-1 is Backwards, +1 is Forwards
 			 jumpDirLeftRight = moveDir.normalized.x; //-1 is Backwards, +1 is Forwards
 
+
+
+			// ------------> removed 10/12/2021 to revert back to original player controller.
 			//	print("Move Dir Forward" + moveDir.normalized.z + "Move Dir Side" + moveDir.normalized.x);
+			/*
 
 			//pressing Up and left / right 
 			if (verticalInput < 0 )
@@ -456,7 +474,7 @@ public class NetworkedPlayerController : MonoBehaviour
 			}
 
 
-
+			*/
 		
 
 			//	jumpDir = 
