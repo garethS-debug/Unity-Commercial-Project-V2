72b032a4d54ac8e814c270e952d37024d3e3bfc6
Claire Peuz <clairepeuz@yahoo.fr>
2021-12-13 10:43:52 +0000

    Level 2 fix bugs

--- Assets/Scripts/Puzzles/Boxes/BoxMovement.cs
+++ Assets/Scripts/Puzzles/Boxes/BoxMovement.cs
@@ -13,7 +13,6 @@ public class BoxMovement : MonoBehaviour
         if (other.gameObject.CompareTag("TargetTrigger"))
         {
             displayKey.AddBox(1);
-            Debug.Log("box +1");
         }
     }
 
@@ -22,7 +21,6 @@ public class BoxMovement : MonoBehaviour
         if (other.gameObject.CompareTag("TargetTrigger"))
         {
             displayKey.AddBox(-1);
-            Debug.Log("box -1");
         }
     }
 }
